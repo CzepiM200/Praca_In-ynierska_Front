4228d0351e12fac7c1338ae3710d642eff18da20
CzepiM200 <czepimm200@gmail.com>
2020-11-20 22:13:16 +0100

    Add profile to homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,12 +2,16 @@ import "./_homePage.scss";
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Header from "../Header/Header";
+import Profile from "./Profile/Profile";
 
 const HomePage = (props) => {
   
   return (
     <section className="home-page">
-      <Header />
+        <Header />
+      <div className="container">
+        <Profile />
+      </div>
     </section>
   );
 };
