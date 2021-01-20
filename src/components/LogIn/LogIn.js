367170cc6ca95fbe80ff008a3a5df865315c13ee
Marcin Czepiela <czepimm200@gmail.com>
2021-01-20 23:31:06 +0100

    Refactor LogIn component

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,9 +1,6 @@
 import "./_logIn.scss";
-import React, { useState } from "react";
-//import { ReactComponent as DecorationIcon } from "../../images/svg/Decoration.svg";
-import { BrowserRouter, Switch, Route  } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import Header from "../Header/Header";
+import React from "react";
+import { Route  } from "react-router-dom";
 import LogInEmail from "./LogInEmail/LogInEmail";
 import LogInMethod from "./LogInMethod/LogInMathod";
 
@@ -11,22 +8,18 @@ import LogInMethod from "./LogInMethod/LogInMathod";
 const LogIn = (props) => {
   const { user, setUser } = props;
 
-  
-
   return (
     <>
-      <Header user={user}/>
       <section className="login">
         <Route exact path="/login">
            <LogInMethod setUser={setUser} user={user}/>
-           
         </Route>
         <Route path="/login/email">
             <LogInEmail setUser={setUser} user={user}/>
         </Route>
         <Route exact path="./google"> 
         </Route>    
-      </section>
+      </section> 
     </>
   );
 };
