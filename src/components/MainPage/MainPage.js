5ea59a9b08ddb326c6dc4f756f08ce667b4f84c7
Marcin Czepiela <czepimm200@gmail.com>
2021-01-20 23:35:48 +0100

    Remove plan component

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -7,6 +7,7 @@ import LogIn from "../LogIn/LogIn";
 import SignUp from "../SignUp/SignUp";
 import Activity from "../Activity/Activity";
 import Place from "../Place/Place";
+import Strava from "../Strava/Strava";
 
 const MainPage = (props) => {
     const {  user, setUser } = props;
@@ -15,19 +16,22 @@ const MainPage = (props) => {
         <BrowserRouter>
           <Switch>
             <Route exact path="/">
-              <HomePage  user={user} setUser={setUser}/>
+              <HomePage user={user} setUser={setUser}/>
             </Route>
             <Route path="/login">
-              <LogIn  user={user} setUser={setUser}/>
+              <LogIn user={user} setUser={setUser}/>
             </Route>
             <Route exact path="/signup">
               <SignUp user={user} setUser={setUser}/>
             </Route>
             <Route path="/activity">
-              <Activity  user={user} setUser={setUser}/>
+              <Activity user={user} setUser={setUser}/>
             </Route>
             <Route path="/places">
-              <Place  user={user} setUser={setUser}/>
+              <Place user={user} setUser={setUser}/>
+            </Route>
+            <Route path="/strava">
+              <Strava user={user} setUser={setUser}/>
             </Route>
           </Switch>
         </BrowserRouter> 
--- src/components/Strava/Strava.js
+++ src/components/Strava/Strava.js
@@ -1,12 +1,14 @@
 import "./_strava.scss";
 import React from "react";
 import { Route  } from "react-router-dom";
+import Header from "../Header/Header";
 
 const Strava = (props) => {
   const { user, setUser } = props;
 
   return (
     <>
+      <Header user={user} setUser={setUser}/>
       <section className="strava">
         <article className="strava__window">
             <div className="strava__window_line"></div>
